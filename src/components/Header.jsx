8a315c482b4b11994e3ff7882398fdf73d2fb51a
uswhiteus <dmitry.belousoov@gmail.com>
2026-09-09 04:23:53 +0700

    Create src/components/Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div>
+        <div className="header-title">Мой контроль</div>
+        <div className="header-subtitle">Контроль привычек</div>
+      </div>
+    </header>
+  );
+}
