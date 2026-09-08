42cfb7795d4617b09e66d5863381df73f94b377a
uswhiteus <dmitry.belousoov@gmail.com>
2026-09-09 04:28:10 +0700

    Create src/pages/Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="page">
+      <h2>Сегодня</h2>
+
+      <p className="page-subtitle">
+        Контролируй привычки, а не они тебя.
+      </p>
+
+      <div className="empty-state">
+        Главная страница готова к подключению.
+      </div>
+    </main>
+  );
+}
