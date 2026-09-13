f9db9caf3449eaba0d995a488838141b64cab10d
uswhiteus <dmitry.belousoov@gmail.com>
2026-09-13 09:20:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState } from "react";
+import { LocalNotifications } from "@capacitor/local-notifications";
 
 import Header from "./components/Header";
 import BottomNav from "./components/BottomNav";
